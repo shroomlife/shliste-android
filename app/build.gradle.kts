3cdca1e9a258d1b1c9bf39006533f78c880dd3d4
shroomlife 🕉️👨‍💻🍄🌈🍅 <robin@shroomlife.de>
2025-10-23 22:50:10 +0200

    added v0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.shroomlife.shliste"
         minSdk = 32
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
